8f019133b45465ff682ce8eb7400f3cad30e0db7
Rudresh17 <rudreshlegend@gmail.com>
2020-12-13 07:53:08 +0530

    goog morning

--- app.js
+++ app.js
@@ -4,11 +4,15 @@ const connectDB= require("./config/db")
 const morgan=require('morgan')
 const exphbs = require('express-handlebars')
 const path=require("path")
+const passport = require('passport')
+const session = require('express-session')
+const MongoStore = require('connect-mongo')(session)
+const mongoose = require('mongoose')
 
 
 // load config
 dotenv.config({path: "./config/config.env"})
-
+require('./config/passport')(passport)
 connectDB()
 const app = express()
 //handlebars
@@ -16,7 +20,22 @@ app.engine(".hbs",exphbs({defaultlayout:"main",extname:".hbs"}))
 app.set("view engine",".hbs")
 app.use(express.static(path.join(__dirname,'public')))
 
+
+//PASSPORT
+app.use(
+    session({
+      secret: 'keyboard cat',
+      resave: false,
+      saveUninitialized: false,
+      store: new MongoStore({ mongooseConnection: mongoose.connection }),
+    })
+)
+  
+  // Passport middleware
+app.use(passport.initialize())
+app.use(passport.session())
 app.use("/",require("./routes/index"))
+app.use('/auth', require('./routes/auth'))
 const PORT= process.env.PORT || 4000
 
 
